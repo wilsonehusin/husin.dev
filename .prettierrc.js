f65a894e9f285d8fbce36e9d69efc53904e737f3
Wilson E. Husin <wilson@husin.dev>
2024-01-22 14:04:21 -0800

    prettier: go template

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,15 @@
 module.exports = {
+  plugins: ["prettier-plugin-go-template"],
   overrides: [
     {
-      files: '**/*.svg',
-      options: { parser: 'html' }
+      files: "**/*.svg",
+      options: { parser: "html" },
     },
-  ]
-}
+    {
+      files: ["*.html"],
+      options: {
+        parser: "go-template",
+      },
+    },
+  ],
+};
